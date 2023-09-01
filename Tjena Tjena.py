95933894d30f59f1f60483fc0520534532a5d5a1
Wille Salomonsson <wisa0917@storsjogymnasiet.se>
2023-09-01 13:25:12 +0200

    Start

--- Tjena Tjena.py
+++ Tjena Tjena.py
@@ -2,7 +2,6 @@ from random import randint
 
 min_variabel = " e Gay"
 betyg = randint(1, 10)
-
 print("Emil" + min_variabel)
 
-print(f"Hur mycket: {betyg}")
+print(f"Hur mycket från 1 till 10: {betyg}")
